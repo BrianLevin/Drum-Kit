0818cd50ed45654d2f593cb3b429cd432e40d40f
BrianLevin <bml201095@gmail.com>
2021-04-13 20:29:09 -0400

    changed the style  of the button by using the this keyward which ideifies the idenity the object it belongs to

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     
+     this.style.color= "white";
 
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
+
+
+    // var audio = new Audio('sounds/tom-1.mp3');
+    // audio.play();
 
 });
 
